d06a21428fd79e5d9277279135eda50a87cc73c3
poornima-reddy <70846516+poornima-alugubelly@users.noreply.github.com>
2022-01-14 10:19:09 +0530

    refactor: updated to ES6+

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+const btnTranslate = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input");
+const outputDiv = document.querySelector("#output");
 
 
 
-var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+const serverURL = "https://api.funtranslations.com/translate/yoda.json"
 
+const getTranslationURL = input => `${serverURL}?text=${input}`
 
-function getTranslationURL(input) {
-    return serverURL + "?" + "text=" + input
-}
-
-function errorHandler(error) {
-    console.log("error occured", error);
-    alert("something wrong with server! try again after some time")
-}
+function  errorHandler(error){
+  console.log(`Error occured: ${error}`);
+  alert("something wrong with server! try again after some time");
+};
 
 
 function clickHandler() {
-    var inputText = txtInput.value; 
+    const inputText = txtInput.value; 
 
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            var translatedText = json.contents.translated;
+            const translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
            })
         .catch(errorHandler)
